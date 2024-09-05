8de69cc82e8fd1cc81752f321660ff4ded39e7ed
Krishna Patel <58154884+kurry55@users.noreply.github.com>
2024-09-04 20:49:00 -0400

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,30 @@
+
+public class Controller {
+	
+	String[] songData;
+	
+	
+	
+	public void insert(String artist, String song) {
+		
+	}
+	
+	public void remove(String artist, String song) {
+		
+	}
+	
+	public String printArtist() {
+		
+	}
+	
+	public String printSong() {
+		
+	}
+	
+	public String printGraph() {
+		
+	}
+
+	
+
+}
